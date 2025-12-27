2683b17d864bee5b64a703ca0c9c8a7c6651b69e
Miguel Alpañez <alpanez.alcalde@gmail.com>
2025-12-27 20:56:29 +0000

    docs: add comment explaining blank import in tzembed.go

--- internal/timezone/tzembed.go
+++ internal/timezone/tzembed.go
@@ -1,3 +1,5 @@
 package timezone
 
+// Import tzdata to embed timezone database for platforms that don't have it installed.
+// This blank import ensures the timezone database is available in the compiled binary.
 import _ "time/tzdata"
